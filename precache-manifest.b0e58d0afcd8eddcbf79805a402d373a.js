b62a0ead951d21c14ba8d16834f909170cbde44b
Suhaib <msuhaibh@gmail.com>
2019-09-07 00:55:11 +0100

    Updates

---
+++ precache-manifest.b0e58d0afcd8eddcbf79805a402d373a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cf9cbb3a515777a63c1403ae930649",
+    "url": "/note-taker-with-speech-api/index.html"
+  },
+  {
+    "revision": "1a989fea701eb0207a38",
+    "url": "/note-taker-with-speech-api/static/css/main.a59ee820.chunk.css"
+  },
+  {
+    "revision": "3fa0a9bb94c87ddd1adf",
+    "url": "/note-taker-with-speech-api/static/js/2.f8b82e56.chunk.js"
+  },
+  {
+    "revision": "1a989fea701eb0207a38",
+    "url": "/note-taker-with-speech-api/static/js/main.342fe46f.chunk.js"
+  },
+  {
+    "revision": "254b69a15158d3396e27",
+    "url": "/note-taker-with-speech-api/static/js/runtime~main.bdb085f0.js"
+  }
+]);
